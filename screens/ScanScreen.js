da1c5eb0d648580adee3eba3350bccc06369dfe6
VishalManjunath <vishal15.dk@gmail.com>
2021-10-04 16:32:48 +0530

    Created a new Expo app

---
+++ screens/ScanScreen.js
@@ -0,0 +1,14 @@
+import * as React from 'react';
+import {View, Text} from 'react-native';
+
+export default class ScanScreen extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            hasCameraPermissions: null,
+            scanned: false,
+            scannedData: '',
+            buttonState: 'normal'
+        }
+    }
+  }
